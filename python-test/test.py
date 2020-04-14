3cb1a9e46c6d392c23e460d58ca88eea6f718ff0
scottmerc <scott@headspin.io>
2020-04-14 11:09:30 -0700

    Updating code

--- python-test/test.py
+++ python-test/test.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as ec
+from selenium.common import exceptions
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -27,7 +28,9 @@ def do_basic_signin(driver):
     from selenium.webdriver.support.ui import WebDriverWait
 
     # navigate to sign in page and click slign in button
+    
     driver.get("https://powerbi.microsoft.com/en-us/landing/signin/")
+    
 
     if "Power BI" not in driver.title:
         raise SeleniumTestException("Did not find 'Power BI' in the page title '{}'".format(driver.title))
@@ -68,13 +71,13 @@ def do_basic_signin(driver):
 
     #################################################
 
-    # driver.find_element_by_xpath("//span[text()='Favorites']").click()
-    # driver.find_element_by_xpath("//span[text()='Recent']").click()
-    # driver.find_element_by_xpath("//span[text()='Apps']").click()
-    # driver.find_element_by_xpath("//span[text()='Shared with me']").click()
+     # favorites
     driver.find_element(By.CSS_SELECTOR, "nav-pane-expander:nth-child(2) nav-pane-button > button").click()
+    # recent
     driver.find_element(By.CSS_SELECTOR, "nav-pane-expander:nth-child(3) span").click()
+    # apps
     driver.find_element(By.CSS_SELECTOR, ".myApps span").click()
+    # shared with me
     driver.find_element(By.CSS_SELECTOR, ".sharedWithMe span").click()
 
     expect_url = 'https://app.powerbi.com/sharedwithme?noSignUpCheck=1'
@@ -84,9 +87,19 @@ def do_basic_signin(driver):
     
     driver.quit()
 
+def find_favorites_button(driver):
+    try:
+        favorites_button = WebDriverWait(driver, 10).until(
+            ec.element_to_be_clickable((By.CSS_SELECTOR, "nav-pane-expander:nth-child(2) nav-pane-button > button"))) 
+    except:
+        print("Exception Occurred")
+        favorites_button = WebDriverWait(driver, 10).until(
+            ec.element_to_be_clickable((By.XPATH, "//span[text()='Favorites']")))
+    return favorites_button
 
 
 if __name__ == "__main__":
     web_driver_url = "https://dev-us-la-0.headspin.io:9090/v0/f26664f6bfc64becac8f4c4819f4b5c2/wd/hub"
     driver = setup(web_driver_url)
     do_basic_signin(driver)
+    
